9e64ce5935f1fa6fb295e4e2bd73a5706292099e
blissrose3333-eng <blissrose3333@gmail.com>
2026-02-17 22:05:55 +0545

    Summer

--- Python lesson1/Python Operators-1/Lesson-3/odd_even.py
+++ Python lesson1/Python Operators-1/Lesson-3/odd_even.py
@@ -1,7 +1,7 @@
 number =int(input("Enter a number:"))
 print("Number to be checked",number)
 
-if number %2==0:
-    print("This is even number.")
-else:
+if number %2!=0:
     print("This is odd number.")
+else:
+    print("This is even number.")
---
+++ Python lesson1/Python Operators-1/Lesson-3/summer_time.py
@@ -0,0 +1,5 @@
+temp=float(input("Enter the temperature in celsius:"))
+if temp >15:
+    print("It is summer timee!!")
+else:
+    print("Oh no its not summer yet...")
