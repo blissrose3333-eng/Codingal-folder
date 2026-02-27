ddb2115ebbfcb802d79b19da40256401c184777e
blissrose3333-eng <blissrose3333@gmail.com>
2026-02-27 21:47:13 +0545

    Reverse

---
+++ Python lesson1/Data Types/Loops/Reverse_a_string.py
@@ -0,0 +1,8 @@
+string =input("Please enter your own String:")
+string2=('')
+
+for i in string:
+    string2= i+string2
+
+print("\n The original String=",string)
+print("The Reversed String=",string2)
