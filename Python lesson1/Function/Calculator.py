64c0747a77c86e03f109d0855f8ecfd6d8c005d5
blissrose3333-eng <blissrose3333@gmail.com>
2026-03-20 21:43:48 +0545

    Calculator

--- Python lesson1/Function/Calculator.py
+++ Python lesson1/Function/Calculator.py
@@ -21,10 +21,10 @@ num_2=int(input("Please enter the second number:"))
 if choice =='a':
     print(num_1,"+",num_2,"=",add(num_1,num_2))
 elif choice =='b':
-    print(num_1,"-",num_2,"=",add(num_1,num_2))
+    print(num_1,"-",num_2,"=",subtract(num_1,num_2))
 elif choice =='c':
-    print(num_1,"*",num_2,"=",add(num_1,num_2))
+    print(num_1,"*",num_2,"=",multiply(num_1,num_2))
 elif choice =='d':
-    print(num_1,"/",num_2,"=",add(num_1,num_2))
+    print(num_1,"/",num_2,"=",divide(num_1,num_2))
 else:
     pint("Invalid choice:")
