9c60e9fe8876b3f5cd6250d664027e4aec397b9f
blissrose3333-eng <blissrose3333@gmail.com>
2026-04-10 22:00:00 +0545

    Math operation

---
+++ Python lesson1/Random_Math/Mathmatical_operations.py
@@ -0,0 +1,16 @@
+import math
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+import math
+print('The foor and ceiling value of 23.56 are:'+str(math.ceil(23.56))+str(math.floor(23.56)))
+
+x=10
+y=-15
+
+print('The value of x after copying the sign from y is:' +str(math.copysign(x,y)))
+
+print('Absolute value of -96 and 56 are:'+str(math.fabs(-96))+','+str(math.fabs(56)))
+
+print('The GCD of 24 and 56:'+ str(math.gcd(24,56)))
