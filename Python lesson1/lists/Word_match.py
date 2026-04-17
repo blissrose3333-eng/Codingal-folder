e10e7dcff004d7eb511f2eda35595af8a2eb5a29
blissrose3333-eng <blissrose3333@gmail.com>
2026-04-17 21:42:12 +0545

    Word match

---
+++ Python lesson1/lists/Word_match.py
@@ -0,0 +1,13 @@
+def match_words(words):
+    ctr = 0
+    list = []
+    for word in words:
+        if len(word)> 1 and word[0]==word[-1]:
+            ctr += 1
+            list.append(word)
+
+    print("List ot words with first and last character same\n",list)
+    return ctr
+
+count = match_words(['abc','cfc','xyz','aba','1221'])
+print=("Number of words having the first and last number same:",count)
